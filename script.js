9d5f381d81ad1d31cc9f69e1f12432de9ffa3357
Sijenyi96 <everlynsijenyi96@gmail.com>
2025-02-13 08:04:01 +0300

    add changes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DO(MContentLoaded',function(){
     const photoGridItems = document.querySelectorAll('.photo-grid-item');
       photoGridItems.forEach((item) => {
          const description = item.querySelector(".description");
-         // 1. Show description on right-click (contextmenu)
+         // Show description on right-click (contextmenu)
         item.addEventListener('contextmenu', (event) => {
         event.preventDefault();
         alert (description.textContent);
@@ -12,25 +12,16 @@ document.addEventListener('DO(MContentLoaded',function(){
             description.style.display = "none"
         }, 3000);
         });
-        // 2. change the border on mouseover and reset on mouseleave
+        // change the border on mouseover and reset on mouseleave
         item.addEventListener("mouseover", () =>{
             item.style.border ="5px solid tomato";
         });
         item.addEventListener("mouseleave", () =>{
             item.style.border ="none";
         });
-        // 3. remove the color box from the DOM
+        //  remove the color box from the DOM
         item.addEventListener("dblclick", () =>{
             item.remove();
         })
     });
-
-    document.querySelectorAll('social.icon').forEach(icon => {
-        icon.addEventListener('click',function(event){
-            event.preventDefault();
-            window.open();
-        })
-    })
-    
-
-    });
+ });
